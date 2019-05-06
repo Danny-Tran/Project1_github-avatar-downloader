8a197672feb3c0c4553648ca40ea1bf851c6b733
Danny-Tran <truong_tran@live.com>
2019-05-06 20:20:26 +0000

    step 7 completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 var request = require('request');
 console.log('Welcome to Github Avatar Downloader!');
 var secret = require('./secret')
+var fs = require('fs')
 
 
 // getRepoContributors("jquery", "jquery", function(err, result) {
@@ -20,7 +21,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     request(options, function(err, res, body) {
       cb(err, body);
       var repos = JSON.parse(body);
-      console.log(repos);
+      
       repos.forEach(function(repo){
           console.log(repo.avatar_url)
       });
@@ -32,6 +33,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
         // console.log("Errors:", err);
         // console.log("Result:", result);
       });
-  
+
+function downloadImageByURL(url, filePath) {
+    request.get(url)               
+    .on('error', function (err) {                                   
+      throw err; 
+    })
+    .on('response', function (response) {                           
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));  
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg")
   
   
