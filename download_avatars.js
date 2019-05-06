8f79d17dea582dfa203e37c9fde4acfdb15bc3bb
Danny-Tran <truong_tran@live.com>
2019-05-06 21:17:45 +0000

    step 10 and completed

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,11 @@ var request = require('request');
 console.log('Welcome to Github Avatar Downloader!');
 var secret = require('./secret.js')
 var fs = require('fs')
-
+var first = process.argv[2];
+var last = process.argv[3]
+if (!first) {
+    console.log("Please enter something")
+}
 
 // getRepoContributors("jquery", "jquery", function(err, result) {
 //     console.log("Errors:", err);
@@ -20,7 +24,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   
     request(options, function(err, res, body) {
         var repos = JSON.parse(body);
-      console.log(repos)
+    //   console.log(repos)
       repos.forEach(function(repo){
         //   console.log(repo.avatar_url)
           downloadImageByURL(repo.avatar_url, "avatar/" + repo.login + ".jpg")
@@ -29,9 +33,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
       });
     };
 
-    getRepoContributors("jquery", "jquery", function(err, result) {
+    getRepoContributors(first, last, function(err, result) {
         // console.log("Errors:", err);
         // console.log("Result:", result);
+
       });
 
     function downloadImageByURL(url, filePath) {
